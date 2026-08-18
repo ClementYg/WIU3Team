9c4c4dc75c8a269a949a75493c4ebcfddd6315fc
Regero <reginaeeteng@gmail.com>
2026-08-18 09:45:39 +0800

    added basic inventory display when display is toggled.

---
+++ Assets/Scripts/Inventory/Display/InventoryDisplay.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InventoryDisplay : MonoBehaviour
+{
+    [Header("Dependencies")]
+    [SerializeField] GameObject inventoryRow1;
+    [SerializeField] GameObject inventoryRow2;
+
+    bool isDisplaying = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isToggleDisplayPressed = InputSystem.actions["Toggle Inventory Display"].WasPressedThisFrame();
+        if (isToggleDisplayPressed)
+        {
+            isDisplaying = !isDisplaying;
+
+            if (isDisplaying)
+            {
+                inventoryRow1.SetActive(true);
+                inventoryRow2.SetActive(true);
+            }
+            if (!isDisplaying)
+            {
+                inventoryRow1.SetActive(false);
+                inventoryRow2.SetActive(false);
+            }
+        }
+    }
+}
