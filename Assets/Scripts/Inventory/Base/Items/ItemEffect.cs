c3b92dfca70e918d09edbea1e7f84f3099e80506
Regero <reginaeeteng@gmail.com>
2026-08-18 13:38:38 +0800

    changed folder structure after Clement's integration.

--- Assets/Scripts/Inventory/Base/Items/ItemEffect.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "ItemEffect", menuName = "Scriptable Objects/Inventory/ItemEffect")]
-public abstract class ItemEffect : ScriptableObject
-{
-    public abstract void Use(GameObject user);
-}
