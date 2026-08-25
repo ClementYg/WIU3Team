1fad48ec6d98e5302c37b8dc67f77ee58b0fed3b
LastKnight <jujuwiwi2008@gmail.com>
2026-08-25 14:53:07 +0800

    Added Elder Mo's quest item

---
+++ Assets/Scripts/Inventory/Items/Effects/KeyOfPurityEffect.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "KeyOfPurityItemEffect", menuName = "ScriptableObjects/Inventory/Effects/KeyOfPurityItemEffect")]
+public class KeyOfPurityEffect : ItemEffect
+{
+    public override void Use(GameObject user, ComponentCache userCache)
+    {
+        
+    }
+}
