a8bab8be27af8e232630b8ede9cdb70ad83dad91
Regero <reginaeeteng@gmail.com>
2026-08-26 20:24:52 +0800

    tutorial system: added book guide item.

--- Assets/Scripts/Inventory/Items/Effects/ItemPageEffect.cs
+++ Assets/Scripts/Inventory/Items/Effects/ItemPageEffect.cs
@@ -1,13 +1,24 @@
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "ItemPageEffect", menuName = "ScriptableObjects/Inventory/Effects/ItemPageEffect")]
-public abstract class ItemPageEffect : ItemEffect
+public class ItemPageEffect : ItemEffect
 {
     [Header("Item Page Effect")]
     public UIPage toDisplay; // Displayed when the item is used
 
     [Header("Event Channels")]
     public EventUIPage onDisplayPageRequestedEvent;
+
+    public override void Use(GameObject user, ComponentCache userCache)
+    {
+        if (toDisplay == null || onDisplayPageRequestedEvent == null)
+        {
+            Debug.LogError("ItemPageEffect: " + name + " has missing reference(s).");
+            return;
+        }
+
+        onDisplayPageRequestedEvent.Raise(toDisplay);
+    }
 
     public void RaiseEvent()
     {
